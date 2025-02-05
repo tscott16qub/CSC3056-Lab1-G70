4f0cd494486d9fe6559cddbdf2007bb9fa183d28
tscott16qub <tscott16@qub.ac.uk>
2025-02-05 14:46:09 +0000

    Add files via upload

---
+++ CSC3056Lab1G70/src/unitsTests/AccountTest.java
@@ -0,0 +1,12 @@
+package unitsTests;
+
+import java.util.Date;
+
+import model.Account;
+
+public class AccountTest {
+	public static void main(String[] args) {
+	Account testaccount = new Account ("60604578", "M-Smith", "Student Account", "2025-02-04");
+	System.out.println(testaccount);
+	}
+}
---
+++ CSC3056Lab1G70/src/unitsTests/TransactionTest.java
@@ -0,0 +1,12 @@
+package unitsTests;
+
+import model.Transaction;
+
+public class TransactionTest {
+
+	public static void main(String[] args) {
+	Transaction testUser = new Transaction("mike","my_passwd", "Mike", "Smith", "07771234567" );
+	System.out.println(testUser);
+	
+	}
+}
