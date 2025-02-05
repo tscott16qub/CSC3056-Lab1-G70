1dd1a6d003cdc14f431bd4d222e0710d3af3ad6e
tscott16qub <tscott16@qub.ac.uk>
2025-02-05 13:56:26 +0000

    Add files via upload

---
+++ Mini-Lab Java Refresher 1- Simple banking app G70/src/unitsTests/AccountTest.java
@@ -0,0 +1,10 @@
+package unitsTests;
+
+import java.util.Date;
+
+import model.Account;
+
+public class AccountTest {
+Account testaccount = new Account ("60604578", "M-Smith", "Student Account", 2025-02-05);
+	System.out.println(testaccount);
+}
---
+++ Mini-Lab Java Refresher 1- Simple banking app G70/src/unitsTests/UserTest.java
@@ -0,0 +1,11 @@
+package unitsTests;
+
+import model.User;
+
+public class UserTest {
+	public static void main(String[] args) {
+	User testUser = new User("mike","my_passwd", "Mike", "Smith", "07771234567" );
+	System.out.println(testUser);
+	
+	}
+}
