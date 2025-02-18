9f413f6f9f4e08fba708e2439487628c5e729d74
tscott16qub <tscott16@qub.ac.uk>
2025-02-18 23:10:12 +0000

    Add files via upload

--- SimpleBankingAppSOLVED.java
+++ SimpleBankingAppSOLVED.java
@@ -6,14 +6,22 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Vector;
 
+import controller.AccountController;
+import controller.UserController;
+
+import java.util.Date;
+
 import model.Account;
 import model.Transaction;
 import model.User;
+import model.Account.AccountType;
 
-public class SimpleBankingApp {
+public class SimpleBankingAppSOLVED {
 	public static Vector<User> users = new Vector<User>();
 	public static Vector<Account> accounts  = new Vector<Account>();
 	public static Vector<Transaction> transactions =  new Vector<Transaction>();
+	
+	 UserController userController = new UserController(users);
 	
 	public static void loadUserData() {
 		// structure of each record: username (email address), password, first_name, last_name, mobile_number
@@ -38,7 +46,7 @@ public class SimpleBankingApp {
 		for  (int i = 0; i < users.size(); i++) 
             System.out.println(users.get(i).toString());	
 		System.out.println();
-	}
+	} 
 	
 	public static void loadAccountData()  {
 		// structure of each record: 
@@ -47,24 +55,25 @@ public class SimpleBankingApp {
 		// in the ideal case, we will read from file or database, but let's hard-code for now
 		Account anAccount;
 		try {
-			anAccount = new Account("5495-1234", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
+			
+			anAccount = new Account("5495-1234", "mike", AccountType.SAVINGS, new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
 			accounts.add(anAccount);
 			
-			anAccount = new Account("5495-1239", "mike", "Standard", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2020"));
+			anAccount = new Account("5495-1239", "mike", AccountType.STANDARD, new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2020"));
 			accounts.add(anAccount);
 
-			anAccount = new Account("5495-1291", "mike", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("21/07/2019"));
+			anAccount = new Account("5495-1291", "mike", AccountType.SAVINGS, new SimpleDateFormat("dd/MM/yyyy").parse("21/07/2019"));
 			accounts.add(anAccount);
 
-			anAccount = new Account("5495-6789", "David.McDonald@gmail.com", "Saving", new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
-			accounts.add(anAccount);
+			anAccount = new Account("5495-6789", "David.McDonald@gmail.com", AccountType.SAVINGS, new SimpleDateFormat("dd/MM/yyyy").parse("20/08/2019"));
+			accounts.add(anAccount); 
 
 		} catch (ParseException e) {			
 			e.printStackTrace();
 		}  
 	}
 	
-	public static void printAllAccounts() {
+	public static void printAllAccounts(AccountController accountController) {
 		System.out.println("There are: " + accounts.size() + " accounts in the system.");
 		//System.out.println("Account_number | username_of_account_holder | account_type | account_opening_date");
 
@@ -72,13 +81,13 @@ public class SimpleBankingApp {
 				"Account #", "username_of_account_holder", "type", "opening_date", "Balance"));
 		System.out.println("--------------------------------------------------------------------------------");
 		
-		for  (int i = 0; i < accounts.size(); i++) 
-            System.out.println(accounts.get(i).toString() + "| $" + getBalance(accounts.get(i).getAccount_number()));
+		 for (Account account : accounts) {
+		        System.out.println(account.toString() + " | $" + accountController.getBalance(account.getAccount_number()));
 		
-		System.out.println();
+		System.out.println();}
 	}
 	
-	public static void addTransaction(String account_number, double amount) { 
+	public static void addTransaction(String account_number, double amount, Date transaction_date) { 
 		Transaction aTransaction =  new Transaction(account_number, amount, Calendar.getInstance().getTime());
 		transactions.add(aTransaction);
 	}
@@ -91,11 +100,31 @@ public class SimpleBankingApp {
 	 * @param account_number
 	 * @return A double value, being the balance of the account
 	 */
-	public static double getBalance(String account_number) {
-		return 0;
-		// TODO
-		
+	
+	AccountController accountController = new AccountController(transactions);
+	double balance = accountController.getBalance("5495-1234");
+	
+	// TODO 12a: Display accounts for a specific user
+	
+	public static void printUserAccounts(String username) {
+	    System.out.println("Accounts for user: " + username);
+	    System.out.println("--------------------------------------");
+
+	    boolean found = false;
+	    for (Account acc : accounts) {
+	        if (acc.getUsername_of_account_holder().equals(username)) {
+	            System.out.println(acc);
+	            found = true;
+	        }
+	    }
+
+	    if (!found) {
+	        System.out.println("No accounts found for this user.");
+	    }
 	}
+	
+	// TODO 12b: Prevent Transactions from Non-existent accounts
+	
 	
 	
 	//////////////////////////////////////////////////////
@@ -108,22 +137,25 @@ public class SimpleBankingApp {
 		loadAccountData();
 		// let's print them all to see if they have been loaded (populated) properly
 		System.out.println("Accounts: initial state, after loading...");
-		printAllAccounts();
+		
+		AccountController accountController = new AccountController(transactions);
+		
+		printAllAccounts(accountController);
 		
 		// let's do some activities on the populated accounts, add transactions, etc.
 		// Deposit: adding a transaction with a positive value
 		// Withdraw: adding a transaction with a negative value
-		addTransaction("5495-1234", -50.21);
+		addTransaction("5495-1234", -50.21, Calendar.getInstance().getTime());
 		System.out.println("Account: after the 1st addTransaction function call...");
-		printAllAccounts();
+		printAllAccounts(accountController);
 		
 		// and some more activities on the accounts
-		addTransaction("5495-1234", 520.00);
-		addTransaction("9999-1111", 21.00); // it seems this account does not exist in the loaded (populated) data, 
+		addTransaction("5495-1234", 520.00, Calendar.getInstance().getTime());
+		addTransaction("9999-1111", 21.00, Calendar.getInstance().getTime()); // it seems this account does not exist in the loaded (populated) data, 
 											// but the addTransaction does not do that check, need to improve that function in future
 		// let's print the accounts and their balance to see if the above transaction have impacted their balances
 		System.out.println("Account: after the 2nd/3rd addTransaction function calls...");
-		printAllAccounts();
+		printAllAccounts(accountController);
 		
 
 	}
