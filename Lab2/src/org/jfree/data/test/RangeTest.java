974e48a49bb7926ef361dcd891ae323c0eb5e364
tscott16qub <tscott16@qub.ac.uk>
2025-02-20 12:47:43 +0000

    Add files via upload

---
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,32 @@
+package org.jfree.data.test;
+
+import junit.framework.TestCase;
+import org.jfree.data.Range;
+import org.junit.*;
+
+public class RangeTest extends TestCase {
+	
+private Range exampleRange;
+
+private Range rangeObjectUnderTest;
+
+@Before
+
+public void setUp() throws Exception {
+
+rangeObjectUnderTest = new Range(-1, 1);
+
+}
+
+
+@After
+
+public void tearDown() throws Exception {
+
+
+}
+
+}
+
+
+
